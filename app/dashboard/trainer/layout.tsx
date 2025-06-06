ce8bd8934669d08b2a5eb898bb0b0f1b032c6922
PranavKumar9529desai <dpranav7745@gmail.com>
2025-06-06 19:09:12 +0530

    fixed someupdates

--- app/dashboard/trainer/layout.tsx
+++ app/dashboard/trainer/layout.tsx
@@ -1,5 +1,8 @@
 import type React from "react";
 import { auth } from "../../(auth)/auth";
+import Image from 'next/image';
+import Link from 'next/link';
+import NotOnboardedImage from '../_components/not-onboarded/_assests/not-onboarded.png';
 
 export default async function layout({
   children,
@@ -13,15 +16,26 @@ export default async function layout({
 }
 const NotMemberOfGym = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Not Member of Gym</h1>
-      <p className="mt-4 text-gray-600">
-        Please contact your trainer for
-        <span className="text-lg font-semibold text-blue-300">
-          {" "}
-          onboarding.
-        </span>
-      </p>
-    </div>
+    <div className="flex flex-col items-center justify-center h-[80vh] space-y-4">
+			<div className="relative w-full max-w-sm h-80  mx-auto">
+				<Image
+					src={NotOnboardedImage}
+					alt="not-member-image"
+					className="object-cover object-center"
+					fill
+				/>
+			</div>
+			<div
+				className="text-center text-lg font-['latin'] font-semibold
+        text-red-600"
+			>
+				<span className="px-4 py-2 ">You're not a member of a gym yet!</span>
+			</div>
+			<div className="text-center bg-green-500 text-white w-60 rounded-lg px--4 py-2 mx-auto ">
+				<Link type="link" className="text-base" href="/onboarding/client">
+					Click here to get Onboarded
+				</Link>
+			</div>
+		</div>
   );
 };
